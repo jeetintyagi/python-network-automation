016e8cb5a1138582941a2dd2aa6de8560fabd921
Jitin Tyagi <jt9999709701@gmail.com>
2022-12-27 14:35:48 +0530

    Added 3-configuring-router.py script

--- src/telnetLibScripts/3-configuring-router.py
+++ src/telnetLibScripts/3-configuring-router.py
@@ -3,11 +3,11 @@ import getpass
 import telnetlib
 
 # hardcoding host ip to HOST variable & telnet authentication
-HOST = "192.168.1.21"
+HOST = "172.22.23.20"
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
-# telnet the host(switch) and asking password from the user
+# telnet the host(router) and asking password from the user
 tn = telnetlib.Telnet(HOST)
 
 tn.read_until(b"Username: ")
@@ -16,16 +16,19 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-# executing configuration on switch -
+# executing configuration on router -
 tn.write(b"enable\n")
 tn.write(b"cisco\n")
 tn.write(b"conf t\n")
-tn.write(b"interface e0/1\n")
-tn.write(b"switchport access vlan 10\n")
-tn.write(b"switchport mode access\n")
-tn.write(b"interface e0/2\n")
-tn.write(b"switchport access vlan 20\n")
-tn.write(b"switchport mode access\n")
+tn.write(b"interface f0/0\n")
+tn.write(b"ip ospf 100 area 0\n")
+
+tn.write(b"interface l0\n")
+tn.write(b"ip ospf 100 area 0\n")
+
+tn.write(b"router ospf 100\n")
+tn.write(b"router-id 1.1.1.1\n")
+
 
 # writing the configuration terminating telnet session with the switch
 tn.write(b"end\n")
