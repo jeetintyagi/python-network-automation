e3802ad66273e7ecd1c48fc0f39432b6eea96d9d
Jitin Tyagi <jt9999709701@gmail.com>
2023-01-03 20:47:32 +0530

    Modified netmiko script #1

--- src/netmiko/1-backup-running-config.py
+++ src/netmiko/1-backup-running-config.py
@@ -1,33 +1,23 @@
-"""
-(1) Import the necessary libraries, such as netmiko and time.
-(2) Define a list of dictionaries, where each dictionary represents 
-a switch and contains the necessary information to connect to it, 
-such as the hostname or IP address, username, password, and device type.
-(3) Iterate over the list of dictionaries and create a "netmiko.ConnectHandler()" 
-object for each switch.
-(4) Connect to the switch using ".connect()" method.
-(5) Send the appropriate commands to retrieve the configuration of the 
-switch using the ".send_command()" method. This may vary depending on the 
-type of switch and the command-line interface(CLI) used.
-(6) Save the configuration to a text file, using the management IP of the 
-switch as the file name.
-(7) Disconnect from the switch using the ".disconnect()" method.
-
-"""
 import netmiko
 
 # List of dictionaries, where each dictionary represents a switch
 switches = [
     {
-        "host": "192.168.1.1",
-        "username": "admin",
-        "password": "password",
+        "host": "172.23.253.31",
+        "username": "david",
+        "password": "cisco",
         "device_type": "cisco_ios",
     },
     {
-        "host": "192.168.1.2",
-        "username": "admin",
-        "password": "password",
+        "host": "172.23.253.32",
+        "username": "david",
+        "password": "cisco",
+        "device_type": "cisco_ios",
+    },
+    {
+        "host": "172.23.253.33",
+        "username": "david",
+        "password": "cisco",
         "device_type": "cisco_ios",
     },
 ]
@@ -37,13 +27,12 @@ for switch in switches:
     # Create a ConnectHandler object
     net_connect = netmiko.ConnectHandler(**switch)
 
-    # Connect to the switch
-    net_connect.connect()
-
     # Send the command to retrieve the configuration
     config = net_connect.send_command("show running-config")
 
-    # Save the configuration to a file
+    print("Backing up running-config of " + switch["host"])
+
+    # Save the running-config to a file
     with open(f"{switch['host']}.txt", "w") as f:
         f.write(config)
 
